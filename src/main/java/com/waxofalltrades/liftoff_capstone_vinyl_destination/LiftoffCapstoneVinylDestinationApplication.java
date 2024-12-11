ff240bab34f9aca2e44080a59f1944cf076510ce
GrizzlyMain <ryan.thom.dalton@gmail.com>
2024-12-10 20:41:57 -0600

    1st Commit

---
+++ src/main/java/com/waxofalltrades/liftoff_capstone_vinyl_destination/LiftoffCapstoneVinylDestinationApplication.java
@@ -0,0 +1,13 @@
+package com.waxofalltrades.liftoff_capstone_vinyl_destination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftoffCapstoneVinylDestinationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftoffCapstoneVinylDestinationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waxofalltrades/liftoff_capstone_vinyl_destination/LiftoffCapstoneVinylDestinationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waxofalltrades.liftoff_capstone_vinyl_destination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftoffCapstoneVinylDestinationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
