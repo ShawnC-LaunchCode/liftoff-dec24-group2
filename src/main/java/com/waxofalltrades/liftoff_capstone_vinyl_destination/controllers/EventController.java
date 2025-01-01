cec553136ffc58961b870f6d9e6efcafc16efe90
jayheid <jp.heidbrink@gmail.com>
2025-01-01 14:28:47 -0600

    displaying add event form

--- src/main/java/com/waxofalltrades/liftoff_capstone_vinyl_destination/controllers/EventController.java
+++ src/main/java/com/waxofalltrades/liftoff_capstone_vinyl_destination/controllers/EventController.java
@@ -1,10 +1,14 @@
 package com.waxofalltrades.liftoff_capstone_vinyl_destination.controllers;
 
+import com.waxofalltrades.liftoff_capstone_vinyl_destination.models.Event;
+import com.waxofalltrades.liftoff_capstone_vinyl_destination.models.Item;
 import com.waxofalltrades.liftoff_capstone_vinyl_destination.repositories.EventRepository;
+import com.waxofalltrades.liftoff_capstone_vinyl_destination.repositories.EventTypeRepository;
 import com.waxofalltrades.liftoff_capstone_vinyl_destination.repositories.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,6 +18,9 @@ public class EventController {
     @Autowired
     private EventRepository eventRepository;
 
+    @Autowired
+    private EventTypeRepository eventTypeRepository;
+
     // Display all Events
     @RequestMapping("/")
     public String displayAllEvents(Model model){
@@ -22,7 +29,14 @@ public class EventController {
         return "event/list";
     }
 
+
     // Add Event
+    @GetMapping("add")
+    public String displayAddEventForm(Model model){
+        model.addAttribute(new Event());
+        model.addAttribute("eventTypes", eventTypeRepository.findAll());
+        return "event/add";
+    }
 
     // Modify Event
 
---
+++ src/main/java/com/waxofalltrades/liftoff_capstone_vinyl_destination/repositories/EventTypeRepository.java
@@ -0,0 +1,7 @@
+package com.waxofalltrades.liftoff_capstone_vinyl_destination.repositories;
+
+import com.waxofalltrades.liftoff_capstone_vinyl_destination.models.EventType;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EventTypeRepository extends CrudRepository<EventType, Integer> {
+}
