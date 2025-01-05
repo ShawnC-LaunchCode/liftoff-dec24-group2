eb869fb7dd96d2e34d8c9b36083e3fd87d3202cd
jayheid <jp.heidbrink@gmail.com>
2025-01-05 15:49:40 -0600

    adding display form and post form submission functionality

---
+++ src/main/java/com/waxofalltrades/liftoff_capstone_vinyl_destination/controllers/ContactFormController.java
@@ -0,0 +1,42 @@
+package com.waxofalltrades.liftoff_capstone_vinyl_destination.controllers;
+
+import com.waxofalltrades.liftoff_capstone_vinyl_destination.models.ContactForm;
+import com.waxofalltrades.liftoff_capstone_vinyl_destination.models.Item;
+import com.waxofalltrades.liftoff_capstone_vinyl_destination.repositories.ContactFormRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "contact")
+public class ContactFormController {
+    @Autowired
+    ContactFormRepository contactFormRepository;
+
+    // Display Contact Form
+    @GetMapping("/")
+    public String displayContactForm(Model model){
+        model.addAttribute(new ContactForm());
+        return "contact";
+    }
+
+    // Post Submission
+    @PostMapping("/")
+    public String processContactForm(@ModelAttribute @Valid ContactForm newContactForm, Errors errors, Model model){
+        if (errors.hasErrors()) {
+            model.addAttribute(new Item());
+            return "contact";
+        }
+
+        contactFormRepository.save(newContactForm);
+        return "redirect:/";
+
+    }
+
+}
