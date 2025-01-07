1c68f20a9389374f312358dfbea64d8e64352fe9
GrizzlyMain <ryan.thom.dalton@gmail.com>
2025-01-07 12:51:36 -0600

    addToCart added

--- src/main/java/com/waxofalltrades/liftoff_capstone_vinyl_destination/controllers/CartController.java
+++ src/main/java/com/waxofalltrades/liftoff_capstone_vinyl_destination/controllers/CartController.java
@@ -1,13 +1,24 @@
 package com.waxofalltrades.liftoff_capstone_vinyl_destination.controllers;
 
+import com.waxofalltrades.liftoff_capstone_vinyl_destination.models.ShoppingCart;
+import com.waxofalltrades.liftoff_capstone_vinyl_destination.repositories.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
 public class CartController {
+    @Autowired
+    private ItemRepository itemRepository;
 
-
+@GetMapping("/cart/{id}")
+    public String addToCart(@PathVariable int id){
+    ShoppingCart.cart.add(itemRepository.findById(id).get());
+    return"redirect:/shop";
+}
 
 
 }
