ed6b996fdf0bd37f7798b403f62d167768f581be
jayheid <jp.heidbrink@gmail.com>
2025-01-18 13:31:35 -0600

    adding functionality to view message for each contact submission in detail template

--- src/main/java/com/waxofalltrades/liftoff_capstone_vinyl_destination/controllers/ContactFormController.java
+++ src/main/java/com/waxofalltrades/liftoff_capstone_vinyl_destination/controllers/ContactFormController.java
@@ -8,14 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Controller
 @RequestMapping(value = "contact")
@@ -48,9 +46,24 @@ public class ContactFormController {
     @GetMapping("list")
     public String displayContactFormSubmission(Model model){
 
-        model.addAttribute("submissions", contactFormRepository.findAll());
+        model.addAttribute("contacts", contactFormRepository.findAll());
         return "contact/list";
     }
 
+    // Display Contact Form Submissions
+    @GetMapping("detail/{contactFormId}")
+    public String displayContactFormSubmissionDetail(Model model, @PathVariable int contactFormId){
+        Optional<ContactForm> currentContactForm = contactFormRepository.findById(contactFormId);
+        if(currentContactForm.isEmpty()){
+            model.addAttribute("heading", "Invalid Contact Form Submission");
+        }
+        else{
+            ContactForm result = currentContactForm.get();
+            model.addAttribute("heading", "Contact Form Submission Detail");
+            model.addAttribute("contact", result);
+        }
+
+        return "contact/detail";
+    }
 
 }
