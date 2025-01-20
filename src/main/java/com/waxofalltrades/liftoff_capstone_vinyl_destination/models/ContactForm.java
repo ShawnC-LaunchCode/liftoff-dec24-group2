0a260618aef31ff4e89d5271e67d907d78d5a0d3
jayheid <jp.heidbrink@gmail.com>
2025-01-20 15:45:31 -0600

    modifying primary key to generate auto-incrementing value

--- src/main/java/com/waxofalltrades/liftoff_capstone_vinyl_destination/models/ContactForm.java
+++ src/main/java/com/waxofalltrades/liftoff_capstone_vinyl_destination/models/ContactForm.java
@@ -13,7 +13,7 @@ import java.util.Date;
 @Entity
 public class ContactForm {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @NotBlank(message = "Name Required")
