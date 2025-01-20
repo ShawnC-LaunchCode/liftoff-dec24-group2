7e0ae7e1b6640fb0a2e529dafad08afd55e30809
jayheid <jp.heidbrink@gmail.com>
2025-01-20 15:46:46 -0600

    changing primary key to be auto incrementing

--- src/main/java/com/waxofalltrades/liftoff_capstone_vinyl_destination/models/Event.java
+++ src/main/java/com/waxofalltrades/liftoff_capstone_vinyl_destination/models/Event.java
@@ -15,7 +15,7 @@ import java.util.Date;
 @Entity
 public class Event {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @NotBlank(message = "Name is required")
