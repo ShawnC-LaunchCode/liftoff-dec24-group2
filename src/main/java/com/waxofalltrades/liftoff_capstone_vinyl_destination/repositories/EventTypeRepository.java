5c14319d19a485882971dee63511eb6af3c41574
jayheid <jp.heidbrink@gmail.com>
2025-01-25 15:05:34 -0600

    adding crud functionality for event type

--- src/main/java/com/waxofalltrades/liftoff_capstone_vinyl_destination/repositories/EventTypeRepository.java
+++ src/main/java/com/waxofalltrades/liftoff_capstone_vinyl_destination/repositories/EventTypeRepository.java
@@ -1,9 +1,13 @@
 package com.waxofalltrades.liftoff_capstone_vinyl_destination.repositories;
 
+import com.waxofalltrades.liftoff_capstone_vinyl_destination.models.Artist;
 import com.waxofalltrades.liftoff_capstone_vinyl_destination.models.EventType;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EventTypeRepository extends CrudRepository<EventType, Integer> {
+    List<EventType> findAllByOrderByNameAsc();
 }
